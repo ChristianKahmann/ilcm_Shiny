96997d5ec8d62ac7962537bcbe22cf774d758d00
Luido <luido@gmx.net>
2020-12-13 10:10:19 +0000

    update documentation

--- server/tab_Create_Annotation_Set.R
+++ server/tab_Create_Annotation_Set.R
@@ -1,19 +1,42 @@
+#' create and save an annotation set
+
+#' add a new category to the top level
+#' depends on:
+#'   input$add_category_top_level: confirm adding a category on top level
+#'   values$anno: annotation value
+#'   values$edit_id: edit id for new anotation
+#'   
 observeEvent(input$add_category_top_level,{
   values$anno <- operateOnAnnotationSystem(annotation_system = anno,operation = "add")
   values$edit_id<-NULL
 },once = F)
 
+#' add a new category
+#' depends on:
+#'   input$add_category: confirm adding a new category
+#'   values$anno: annotation values
+#'   values$edit_id: edit id for new annotation
 observeEvent(input$add_category,{
   values$anno <- operateOnAnnotationSystem(parent_id = input$add_category$parent_id, annotation_system = anno, operation = "add")
   values$edit_id<-NULL
 },once = F)
 
+#' delete a category
+#' depends on:
+#'   input$delete_category: confirm to delete a category
+#'   
 observeEvent(input$delete_category,{
   
   shinyWidgets::confirmSweetAlert(session = session,inputId = "confirm_delete",type = "warning",title = "Are you sure you want to delete this Category",danger_mode = T)
   
 },once = F)
 
+#' confirm that an element should be deleted
+#' depends on:
+#'   input$confirm_delete: button to confrim to delete an element is selected
+#'   input$delete_category: delete a category
+#'   values$anno: selected annotation
+#'   values$edit_id: edited id of selected element
 observeEvent(input$confirm_delete,{
   if(isTRUE(input$confirm_delete)){
     values$anno <- operateOnAnnotationSystem(parent_id = input$delete_category$parent_id, annotation_system = anno, operation = "delete")
@@ -21,16 +44,31 @@ observeEvent(input$confirm_delete,{
   }
 })
 
+#' sort all categories in ascending sequence
+#' depends on:
+#'   values$anno: vales of annotation
+#'   input$sort_category_up: initiate sorting the elements in ascending sequence
+#'   values$edit_id: edit id of selected annotation
 observeEvent(input$sort_category_up,{
   values$anno <- operateOnAnnotationSystem(parent_id = input$sort_category_up$parent_id, annotation_system = anno, operation = "sort_up")
   values$edit_id<-NULL
 },once = F)
 
+#' sort all categories downward
+#' depends on:
+#'   values$anno: vales of annotation
+#'   input$sort_category_up: initiate sorting the elements downward
+#'   values$edit_id: edit id of selected annotation
 observeEvent(input$sort_category_down,{
   values$anno <- operateOnAnnotationSystem(parent_id = input$sort_category_down$parent_id, annotation_system = anno, operation = "sort_down")
   values$edit_id<-NULL
 },once = F)
 
+#' save a category
+#' depends on:
+#'   input$save_category: save all information from category
+#'   values$edit_id: edit id of chosen element
+#'   values$anno: define annotation of category
 observeEvent(input$save_category,{
   
   myElement <- list()
@@ -42,6 +80,7 @@ observeEvent(input$save_category,{
   values$anno <- operateOnAnnotationSystem(parent_id = input$save_category$parent_id, annotation_system = anno, operation = "save_edit", updateElement = myElement)
 },once = F)
 
+#' 
 observeEvent(input$cancel_category,{
   #Re-Render with
   values$edit_id<-NULL
