f115dcee1a4ebf8d1c5a1aa6e0f1174e43666cb2
ChristianKahmann <christiangoldlauter@googlemail.com>
2023-06-01 14:46:25 +0000

    version number in information tab updated

--- config_file.R
+++ config_file.R
@@ -3,7 +3,7 @@ url<-'http://0.0.0.0:8983/solr/iLCM/select/'
 update_solr_url<-'0.0.0.0'
 update_solr_port<-'8983'
 host<-'0.0.0.0'
-version<-"0.999"
+version<-"1.01"
 db_port='3306'
 max_upload_file_size=500
 random_seed=1236
